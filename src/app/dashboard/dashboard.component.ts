ec735d533d469283d53662dc58d4e4f93820fdd7
yogesh <yogeshphatangare99@gmail.com>
2019-01-17 19:02:22 +0530

    added more changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { LayoutService } from '../services/layout.service';
 
 @Component({
   selector: 'weather-dashboard',
@@ -9,29 +6,5 @@ import { LayoutService } from '../services/layout.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver,
-    private layoutService: LayoutService ) {
-     // this.layoutService.updateLayout({ sidenav: 'hide', toolbar: 'show' });
-    }
 }
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'weather-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss']
 })
-export class FirstComponent implements OnInit {
+export class FirstComponent {
 
-  constructor() { }
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
 
-  ngOnInit() {
+      // if (matches) {
+      //   return [
+      //     { title: 'Today', cols: 1, rows: 1 },
+      //     { title: 'Card 2', cols: 1, rows: 1 },
+      //     { title: 'Card 3', cols: 1, rows: 1 },
+      //     { title: 'Card 4', cols: 1, rows: 1 }
+      //   ];
+      // }
+
+      return [
+        { title: 'Today', cols: 1, rows: 1 },
+        { title: 'tomorrow', cols: 1, rows: 1 },
+        { title: 'This Month', cols: 2, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
   }
 
+  getBackgroundImageUrl(url: string): string {
+
+    return `${environment.assestsUrl}/img/${url}`;
+  }
 }
